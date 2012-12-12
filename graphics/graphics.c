b5e4e86d5d3d42b181d976e4bcd2a155bd9444cc
A. Samuel Pottinger <Samnsparky@gmail.com>
2012-12-12 02:12:58 -0700

    Moved constants around.

--- graphics/graphics.c
+++ graphics/graphics.c
@@ -26,60 +26,6 @@
 #include "sky.h"
 #include "tree.h"
 
-#define TITLE "Boids Simulation"
-
-#define WIDTH 600
-#define HEIGHT 500
-
-#define PERSPECTIVE 1
-#define ORTHOGRAPHIC 2
-#define FIRST_PERSON 3
-#define THIRD_PERSON 4
-
-#define TRUE 1
-#define FALSE 0
-
-#define MAX_BALLOON_HEIGHT 300
-#define MIN_BALLOON_HEIGHT 50
-
-#define NUM_BIRDS 100
-
-#define NUM_TREES 150
-
-#define FPS_FORWARD_AMOUNT 5
-
-#define UPDATE_FREQUENCY 25
-
-#define THIRD_PERSON_CAMERA_Y_OFFEST -20
-#define THIRD_PERSON_CAMERA_Z_OFFEST -50
-
-#define BALLOON_2_ROT_RATE 1.25
-#define BALLOON_3_ROT_RATE 1.5
-#define BALLOON_4_ROT_RATE 1.75
-
-#define BALLOON_2_Y_MOVE_RATE 0.2
-#define BALLOON_3_Y_MOVE_RATE 0.3
-#define BALLOON_4_Y_MOVE_RATE 0.4
-
-#define LIGHT_ROT_RATE 0.03
-#define LIGHT_ROT_RADIUS 320
-
-#define SKY_TEXTURES_START_INDEX 2
-#define TREE_TEXTURES_START_INDEX 8
-
-#define CRATE_TEXTURE_INDEX 0
-#define GROUND_TEXTURE_INDEX 1
-#define SKY_TOP_TEXTURE_INDEX 2
-#define SKY_LEFT_TEXTURE_INDEX 3
-#define SKY_BACK_TEXTURE_INDEX 4
-#define SKY_RIGHT_TEXTURE_INDEX 5
-#define SKY_FRONT_TEXTURE_INDEX 6
-#define SKY_BOTTOM_TEXTURE_INDEX 7
-#define TREE_BARK_INDEX 8
-#define TREE_LEAVES_INDEX 9
-
-#define NUM_TEXTURES 10
-
 // Camera positions
 float xpos = 0, ypos = 50, zpos = 0, xrot = 0, yrot = 0, angle=0.0;
 
